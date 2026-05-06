d3288a7ac2afcd4ad71b98015595b1843a0e2a20
moizmemon12340-sudo <moizmemon12340@gmail.com>
2026-05-06 08:28:25 +0530

    index.py

--- index.py
+++ index.py
@@ -3,19 +3,26 @@ import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "BGMI API Server is Active!"
+
 @app.route('/api/bgmi/<uid>')
-def get_username(uid):
+def get_bgmi_user(uid):
+    # RapidAPI URL with dynamic UID
     url = f"https://id-game-checker.p.rapidapi.com/bgmi/{uid}"
+    
     headers = {
         "x-rapidapi-host": "id-game-checker.p.rapidapi.com",
-        "x-rapidapi-key": "APKI_RAPID_API_KEY"
+        "x-rapidapi-key": "4031d8fca9mshd856dbf4ba5f5e5p1ad3bejsnd6dbde6aa518"
     }
+
     try:
         response = requests.get(url, headers=headers)
+        # Seedha JSON return karega jo RapidAPI se aayega
         return jsonify(response.json())
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Ye line local testing ke liye hai, Vercel ise ignore kar dega
-if __name__ == "__main__":
-    app.run()
+# Vercel ko iski zaroorat hoti hai
+app.run()
